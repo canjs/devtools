b5ed9cca9f605edeef99d52dacdcd9faca9d4cb8
Kevin Phillips <kevin@bitovi.com>
2018-04-10 17:08:30 -0500

    refactoring ViewModel sidebar to use inspectedWindow.eval and polling
    This will allow ViewModel data to update as stepping through the
    debugger. It also greatly simplifies the code since we don't need to
    inject any scripts into the user's page.

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onConnect.addListener(function(port) {
-    // listen to messages from the canjs-devtools.js
-    port.onMessage.addListener(function(msg) {
-        // pass messages through to extension (panels, sidebar, etc)
-        chrome.runtime.sendMessage(msg);
-    });
-});
--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -1,47 +1,84 @@
+// run an expression in the page
 var pageEval = chrome.devtools.inspectedWindow.eval;
 
-var contentScriptEval = function(jsString, callback) {
-    return pageEval(
-        jsString,
-        { useContentScriptContext: true },
-        callback
-    );
+var POLLING_INTERVAL = 100;
+
+// helpers for working with the selected element $0
+var selectedElement = {
+    displayData: function displayData(elementData) {
+        selectedElement.getData()
+            .then(function(elementData) {
+                var tagName = elementData.tagName;
+                var viewModel = elementData.viewModel;
+
+                var header = document.createElement("h1");
+                var headerText = document.createTextNode("<" + tagName.toLowerCase() + "> ViewModel");
+                header.appendChild(headerText);
+
+                document.body.innerHTML = "";
+                document.body.appendChild(header);
+
+                for (var key in viewModel) {
+                    var p = document.createElement("p");
+                    var keyText = document.createTextNode(key + ": ");
+                    var valueText = document.createTextNode(viewModel[key]);
+
+                    p.appendChild(keyText);
+                    p.appendChild(valueText);
+
+                    document.body.appendChild(p);
+                }
+
+                return elementData;
+            })
+            .then(function(elementData) {
+                // poll for changes
+                setTimeout(selectedElement.displayData, POLLING_INTERVAL);
+            });
+    },
+
+    getTagName: function getTagName() {
+        return new Promise(function(resolve, reject) {
+            var viewModel;
+
+            pageEval("$0.tagName", function(result, isException) {
+                if (isException) {
+                    reject(isException);
+                }
+                resolve(result.toLowerCase());
+            });
+        });
+    },
+
+    getViewModel: function getViewModel() {
+        return new Promise(function(resolve, reject) {
+            pageEval("can.viewModel($0).serialize()", function(result, isException) {
+                if (isException) {
+                    reject(isException);
+                }
+                resolve(result);
+            });
+        });
+    },
+
+    getData: function getData() {
+        return Promise.all([
+            selectedElement.getTagName(),
+            selectedElement.getViewModel()
+        ])
+        .then(function(data) {
+            var tagName = data[0];
+            var viewModel = data[1];
+
+            return {
+                tagName: tagName,
+                viewModel: viewModel
+            };
+        });
+    }
 };
 
-// set up handler for when element selected in devtools changes
-chrome.devtools.panels.elements.onSelectionChanged.addListener(
-    function selectedElementChangeHandler() {
-        contentScriptEval("getViewModelData($0)");
-    }
-);
-
-function displayViewModelData(tagName, viewModel) {
-    var header = document.createElement("h1");
-    var headerText = document.createTextNode("<" + tagName.toLowerCase() + "> ViewModel");
-    header.appendChild(headerText);
-
-    document.body.innerHTML = "";
-    document.body.appendChild(header);
-
-    for (var key in viewModel) {
-        var p = document.createElement("p");
-        var keyText = document.createTextNode(key + ": ");
-        var valueText = document.createTextNode(viewModel[key]);
-
-        p.appendChild(keyText);
-        p.appendChild(valueText);
-
-        document.body.appendChild(p);
-    }
-}
-
-chrome.runtime.onMessage.addListener(function(msg, sender) {
-    switch(msg.type) {
-        case "viewModel":
-            displayViewModelData(msg.tagName, msg.viewModel);
-            break;
-        default:
-            console.log("unknown message:", JSON.stringify(msg));
-            break;
-    }
-});
+// on load, display the data for the selected element
+// this will set up polling so data will change when selected element changes
+// or element's data (viewModel, etc) changes
+selectedElement.displayData();
