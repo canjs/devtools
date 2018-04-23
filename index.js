3f45943fe365bb9b134eab96997fef5a3423b2ef
Kevin Phillips <kevin@bitovi.com>
2018-04-23 15:09:13 -0500

    re-initializing devtools panels when user navigates to a new page

--- index.js
+++ index.js
@@ -6,45 +6,67 @@ var ifGlobals = function(globals, cb) {
     chrome.devtools.inspectedWindow.eval(
         command,
         function(result, isException) {
-            if (!isException && result) {
-                cb();
-            }
+            cb(!isException && result);
         }
     )
 };
 
-ifGlobals(
-    [ "can", "can.Symbol", "can.viewModel", "can.Reflect" ],
-    function() {
-        chrome.devtools.panels.elements.createSidebarPane("CanJS ViewModel",
-            function initializeSidebar(sidebar) {
-                sidebar.setPage("viewmodel-editor/index.html");
-            }
-        );
-    }
-);
+var displayedSidebars = {};
 
-ifGlobals(
-    [ "can", "can.queues", "can.Reflect" ],
-    function() {
-        // create can-queues stack sidebar
-        chrome.devtools.panels.sources.createSidebarPane("CanJS Queues Stack",
-            function initializeQueuesPanel(sidebar) {
-                sidebar.setPage("queues-stack/index.html");
-                sidebar.setHeight("50vh");
-            }
-        );
-    }
-);
+var initializeSidebar = function(name, globals, location, indexPage, errorPage) {
+    ifGlobals(globals, function(globalsExist) {
+        // if the sidebar was already created, set it to the correct page
+        // if it was not created, only create it if on a page with the `can` globals
+        var existingSidebar = displayedSidebars[name]
+        if (existingSidebar) {
+            existingSidebar.setPage(globalsExist ? indexPage : errorPage);
+        } else {
+            if (globalsExist) {
+                chrome.devtools.panels[location].createSidebarPane(name, function(sidebar) {
+                    sidebar.setPage(indexPage);
 
-ifGlobals(
-    [ "can", "can.Symbol", "can.Reflect", "can.viewModel", "can.debug", "can.debug.formatGraph", "can.debug.getGraph" ],
-    function() {
-        // Add Bindings Graph sidebar
-        chrome.devtools.panels.elements.createSidebarPane("CanJS Bindings Graph",
-            function initializeQueuesPanel(sidebar) {
-                sidebar.setPage("bindings-graph/index.html");
+                    // store sidebar so the page can be updated if the user navigates to a page without the required globals
+                    displayedSidebars[name] = sidebar;
+                });
             }
-        );
-    }
-);
+        }
+    })
+};
+
+var initialize = function() {
+    initializeSidebar(
+        "CanJS ViewModel",
+        [ "can", "can.Symbol", "can.viewModel", "can.Reflect" ],
+        "elements",
+        "viewmodel-editor/index.html",
+        "viewmodel-editor/error.html"
+    );
+
+    initializeSidebar(
+        "CanJS Bindings Graph",
+        [ "can", "can.Symbol", "can.Reflect", "can.viewModel", "can.debug", "can.debug.formatGraph", "can.debug.getGraph" ],
+        "elements",
+        "bindings-graph/index.html",
+        "bindings-graph/error.html"
+    );
+
+    initializeSidebar(
+        "CanJS Queues Stack",
+        [ "can", "can.queues", "can.Reflect" ],
+        "sources",
+        // needs to have min-height of 50vh
+        "queues-stack/index.html",
+        "queues-stack/error.html"
+    );
+};
+
+// initialize the sidebar panels
+initialize();
+
+// if user navigates to a new page, reload devtools sidebars
+// so that data loaded in `connectedCallback`s will be refreshed
+// and the sidebars will be correctly enabled/disabled based on `can` global
+chrome.devtools.network.onNavigated.addListener(function() {
+    // wait for new page to (hopefully) load before re-initializing panels
+    setTimeout(initialize, 2000);
+});
