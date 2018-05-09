f7dbebed65079776e280903a3470eddf8575a291
Kevin Phillips <kevin@bitovi.com>
2018-05-09 16:36:13 -0400

    fixing spaces

--- background.js
+++ background.js
@@ -9,12 +9,12 @@ function sendFrameURLs() {
     setTimeout(sendFrameURLs, 2000);
 }
 
-chrome.runtime.onConnect.addListener(function(port) {	
+chrome.runtime.onConnect.addListener(function(port) {
     if (port.name === "canjs-devtools") {
-        // listen to messages from the canjs-devtools.js	
-        port.onMessage.addListener(function(msg, port) {	
+        // listen to messages from the canjs-devtools.js
+        port.onMessage.addListener(function(msg, port) {
             frameURLs.add( port.sender.url );
-        });	
+        });
     }
 
     sendFrameURLs();
