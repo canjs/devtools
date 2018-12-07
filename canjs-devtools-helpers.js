940ed0825ccb9dabb6a42294086509a25feb3fdb
Kevin Phillips <kevin@bitovi.com>
2018-12-07 13:58:11 -0600

    only refreshing eval'd data _after_ previous request responds

--- canjs-devtools-helpers.js
+++ canjs-devtools-helpers.js
@@ -3,32 +3,67 @@ window.CANJS_DEVTOOLS_HELPERS = {
     // have a global `can` present
     registeredFrames: {},
 
-    runDevtoolsFunction: function(fnString, cb) {
-        var registeredFrames = this.registeredFrames;
+    runDevtoolsFunction: function(options) {
+        var devtoolsHelpers = this;
+        var timeoutIds = [];
+        var keepRefreshing = true;
 
-        var frameURLs = Object.keys(this.registeredFrames);
+        var runDevtoolsFunctionForUrl = function(url) {
+            var refreshDataForThisUrl = function() {
+                if (keepRefreshing && options.refreshInterval) {
+                    var timeoutId = setTimeout(function() {
+                        runDevtoolsFunctionForUrl(url);
+                    }, options.refreshInterval);
 
-        for (var i=0; i<frameURLs.length; i++) {
+                    timeoutIds.push(timeoutId);
+                }
+            };
+
             chrome.devtools.inspectedWindow.eval(
-                "typeof __CANJS_DEVTOOLS__ === 'object' && __CANJS_DEVTOOLS__." + fnString,
-                { frameURL: frameURLs[i] },
+                "typeof __CANJS_DEVTOOLS__ === 'object' && __CANJS_DEVTOOLS__." + options.fnString,
+                { frameURL: url },
                 function(result, exception) {
                     if (exception) {
                         // if there was an exception because we sent a message to a frame
                         // that no longer exists, remove that frame's URL from registeredFrames
                         // to prevent more exceptions before the next _UPDATE_FRAMES_ message
                         if (exception.code === "E_NOTFOUND") {
-                            delete registeredFrames[ exception.details[0] ];
+                            delete devtoolsHelpers.registeredFrames[ exception.details[0] ];
                         }
+                        refreshDataForThisUrl();
                         return;
                     }
 
-                    if (cb) {
-                        cb(result);
+                    if (options.success) {
+                        options.success(result);
                     }
+
+                    refreshDataForThisUrl();
                 }
             );
-        }
+        };
+
+        (function runDevtoolsFunctionForAllUrls() {
+            var frameURLs = Object.keys(devtoolsHelpers.registeredFrames);
+
+            if (frameURLs.length) {
+                frameURLs.forEach(function(url) {
+                    runDevtoolsFunctionForUrl(url);
+                });
+            } else {
+                timeoutIds.push(
+                    setTimeout(runDevtoolsFunctionForAllUrls, 500)
+                );
+            }
+        }());
+
+        return function() {
+            keepRefreshing = false;
+
+            timeoutIds.forEach((id) => {
+                clearTimeout(id);
+            });
+        };
     }
 };
 
--- queues-stack/queues-stack.mjs
+++ queues-stack/queues-stack.mjs
@@ -20,60 +20,54 @@ Component.extend({
         activeFrame: "string",
 
         inspectTask(taskIndex) {
-            window.CANJS_DEVTOOLS_HELPERS.runDevtoolsFunction(
-                "inspectTask(" + taskIndex + ")"
-            );
+            window.CANJS_DEVTOOLS_HELPERS.runDevtoolsFunction({
+                fnString: "inspectTask(" + taskIndex + ")"
+            });
         },
 
         connectedCallback() {
-            var timeoutId;
             var vm = this;
 
-            var updateStack = function() {
-                window.CANJS_DEVTOOLS_HELPERS.runDevtoolsFunction(
-                    "queuesStack()",
-                    function(result) {
-                        var status = result.status;
-                        var stack = result.detail.stack;
-                        var frameURL = result.detail.frameURL;
+            var stopRefreshing = window.CANJS_DEVTOOLS_HELPERS.runDevtoolsFunction({
+                fnString: "queuesStack()",
+                refreshInterval: 100,
+                success: function(result) {
+                    var status = result.status;
+                    var stack = result.detail.stack;
+                    var frameURL = result.detail.frameURL;
 
-                        switch(status) {
-                            case "ignore":
-                                break;
-                            case "error":
-                                vm.error = detail;
-                                break;
-                            case "success":
-                                // if response is received from a frame other than the
-                                // last `activeFrame`, only overwrite data if there is
-                                // data on the stack
-                                var shouldUpdate =
-                                    (
-                                        frameURL === vm.activeFrame && // apply updates from the `activeFrame`
+                    switch(status) {
+                        case "ignore":
+                            break;
+                        case "error":
+                            vm.error = detail;
+                            break;
+                        case "success":
+                            // if response is received from a frame other than the
+                            // last `activeFrame`, only overwrite data if there is
+                            // data on the stack
+                            var shouldUpdate =
+                                (
+                                    frameURL === vm.activeFrame && // apply updates from the `activeFrame`
                                         stack.length !== vm.stack.length // if the length is the same, we assume the list is the same so that sidebar doesn't flash
-                                    ) ||
-                                    (
-                                        frameURL !== vm.activeFrame && // apply updates from another frame only
+                                ) ||
+                                (
+                                    frameURL !== vm.activeFrame && // apply updates from another frame only
                                         stack.length !== 0 // if there are items on the stack
-                                    );
+                                );
 
-                                if (shouldUpdate) {
-                                    vm.activeFrame = frameURL;
-                                    vm.stack = stack;
-                                }
+                            if (shouldUpdate) {
+                                vm.activeFrame = frameURL;
+                                vm.stack = stack;
+                            }
 
-                                break;
-                        }
+                            break;
                     }
-                );
-
-                timeoutId = setTimeout(updateStack, 100);
-            };
-
-            updateStack();
+                }
+            });
 
             return function disconnect() {
-                clearTimeout(timeoutId);
+                stopRefreshing();
             };
         }
     }
